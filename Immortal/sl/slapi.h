33a1406fd41e621cf93c92f861fcd9b476eb6891
Wu Jianhua <toqsxw@outlook.com>
2021-09-25 02:10:43 +0800

    slapi -> Add Cyclic Redundancy Check
    Signed-off-by: Wu Jianhua <toqsxw@outlook.com>

--- Immortal/sl/slapi.h
+++ Immortal/sl/slapi.h
@@ -89,4 +89,25 @@ inline constexpr bool typeof()
     }
     return false;
 }
+
+
+static uint32_t CyclicRedundancyCheck32(const uint8_t *message, uint32_t length)
+{
+    static constexpr uint32_t CRC32_GENERATOR_POLYNOMIAL = 0x04C11DB7;
+    uint32_t CRC = (uint32_t)~0;
+
+    for (int i = 0; i < length; i++)
+    {
+        CRC ^= (((uint32_t)message[i]) << 24);
+        for (int j = 0; j < 8; j++)
+        {
+            uint32_t MSB = CRC >> 31;
+            CRC <<= 1;
+            CRC ^= (0 - MSB) & CRC32_GENERATOR_POLYNOMIAL;
+        }
+    }
+    
+    return CRC;
+}
+
 }
