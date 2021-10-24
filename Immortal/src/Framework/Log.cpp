d41519f5baf6213305cfbd59fc5ca901f297c4bc
Wu Jianhua <toqsxw@outlook.com>
2021-10-25 00:39:04 +0800

    Log: use async logger
    Signed-off-by: Wu Jianhua <toqsxw@outlook.com>

--- Immortal/src/Framework/Log.cpp
+++ Immortal/src/Framework/Log.cpp
@@ -1,24 +1,27 @@
 #include "impch.h"
 #include "Log.h"
 
+#include <spdlog/async.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
 #include <spdlog/sinks/basic_file_sink.h>
+#include <spdlog/sinks/rotating_file_sink.h>
 
 namespace Immortal
 {
+
 std::shared_ptr<spdlog::logger> LOG::logger;
 
 void LOG::INIT()
 {
+    spdlog::init_thread_pool(8192, 1);
     std::vector<spdlog::sink_ptr> logSinks;
     logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
-    logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("Immortal.log", true));
+    logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("logs/Immortal.log"));
 
     logSinks[0]->set_pattern("%n: [%^%l%$][%T]: %v");
     logSinks[1]->set_pattern("[%T] [%l] %n: %v");
 
-    logger = std::make_shared<spdlog::logger>("Immortal", begin(logSinks), end(logSinks));
-    spdlog::register_logger(logger);
+    logger = std::make_shared<spdlog::async_logger>("Immortal", logSinks.begin(), logSinks.end(), spdlog::thread_pool(), spdlog::async_overflow_policy::block);
     logger->set_level(spdlog::level::trace);
     logger->flush_on(spdlog::level::trace);
 }
