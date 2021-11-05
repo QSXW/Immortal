32b42d90d6d7f60e2ad602c2ce91300dd5192645
Wu Jianhua <toqsxw@outlook.com>
2021-11-06 03:02:33 +0800

    Render/Texture: add FormatSize() to Description
    Signed-off-by: Wu Jianhua <toqsxw@outlook.com>

--- Immortal/src/Render/Texture.h
+++ Immortal/src/Render/Texture.h
@@ -89,6 +89,11 @@ public:
             return Map::BaseFormat<T>(Format);
         }
 
+        auto FormatSize() const
+        {
+            return Map::FormatSize(Format);
+        }
+
         auto ComponentCount() const
         {
             return Map::FormatComponentCount(Format);
