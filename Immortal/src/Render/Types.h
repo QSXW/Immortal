39957ad915a8cf4fdf5844a1b541d746f1346b04
Wu Jianhua <toqsxw@outlook.com>
2021-12-01 23:50:09 +0800

    Render: add Types for common usage
    Signed-off-by: Wu Jianhua <toqsxw@outlook.com>

---
+++ Immortal/src/Render/Types.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "ImmortalCore.h"
+
+namespace Immortal
+{
+
+union Colour
+{
+    Vector4     v4;
+    float       float32[4];
+    int32_t     int32[4];
+    uint32_t    uint32[4];
+
+    Colour(Vector4 v) :
+        v4{ v }
+    {
+    
+    }
+
+    Colour(float r, float g, float b, float a) :
+        float32{ r, g, b, a }
+    {
+
+    }
+};
+
+struct DepthStencilValue
+{
+    float depth;
+    uint32_t stencil;
+};
+
+}
