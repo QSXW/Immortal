7ba98385984e4becd6d16a7bdac84fb4dfe6f35a
Wu Jianhua <toqsxw@outlook.com>
2021-12-10 00:50:32 +0800

    Vulkan/Common: move pool size from device to common
    Signed-off-by: Wu Jianhua <toqsxw@outlook.com>

--- Immortal/src/Platform/Vulkan/Device.cpp
+++ Immortal/src/Platform/Vulkan/Device.cpp
@@ -195,21 +195,7 @@ Device::Device(PhysicalDevice &physicalDevice, VkSurfaceKHR surface, std::unorde
 
     fencePool.reset(new FencePool{ this });
 
-    std::vector<VkDescriptorPoolSize> poolSize{
-        { VK_DESCRIPTOR_TYPE_SAMPLER,                1000 },
-        { VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, 1000 },
-        { VK_DESCRIPTOR_TYPE_SAMPLED_IMAGE,          1000 },
-        { VK_DESCRIPTOR_TYPE_STORAGE_IMAGE,          1000 },
-        { VK_DESCRIPTOR_TYPE_UNIFORM_TEXEL_BUFFER,   1000 },
-        { VK_DESCRIPTOR_TYPE_STORAGE_TEXEL_BUFFER,   1000 },
-        { VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER,         1000 },
-        { VK_DESCRIPTOR_TYPE_STORAGE_BUFFER,         1000 },
-        { VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER_DYNAMIC, 1000 },
-        { VK_DESCRIPTOR_TYPE_STORAGE_BUFFER_DYNAMIC, 1000 },
-        { VK_DESCRIPTOR_TYPE_INPUT_ATTACHMENT,       1000 }
-    };
-
-    descriptorPool = std::make_unique<DescriptorPool>(this, poolSize);
+    descriptorPool.reset(new DescriptorPool{ this, Limit::PoolSize });
 
     EnableGlobal();
 }
