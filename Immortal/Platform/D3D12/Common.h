e11b30b416f21aa6591c75f7fed8e35f8029ac3f
Wu Jianhua <toqsxw@outlook.com>
2021-12-17 01:36:54 +0800

    D3D12: add Resource class
    Signed-off-by: Wu Jianhua <toqsxw@outlook.com>

--- Immortal/Platform/D3D12/Common.h
+++ Immortal/Platform/D3D12/Common.h
@@ -57,6 +57,7 @@ enum DisplayCurve
     None
 };
 
+#define D3D12_GPU_VIRTUAL_ADDRESS_NULL    ((D3D12_GPU_VIRTUAL_ADDRESS)0)
 #define D3D12_GPU_VIRTUAL_ADDRESS_UNKNOWN ((D3D12_GPU_VIRTUAL_ADDRESS)-1)
 
 static const float HDRMetaDataPool[4][4] = {
---
+++ Immortal/Platform/D3D12/Resource.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "Common.h"
+
+namespace Immortal
+{
+namespace D3D12
+{
+
+class Resource
+{
+public:
+    struct Description : public D3D12_RESOURCE_DESC
+    {
+
+    };
+
+public:
+    virtual ~Resource()
+    {
+        Destory();
+    }
+
+    virtual void Destory()
+    {
+        IfNotNullThenRelease(resource);
+        gpuVirtualAddress = D3D12_GPU_VIRTUAL_ADDRESS_NULL;
+    }
+
+    void Set(const std::wstring &name)
+    {
+        resource->SetName(name.c_str());
+    }
+
+    bool Ready()
+    {
+        return !!resource;
+    }
+
+protected:
+    ID3D12Resource *resource{ nullptr };
+
+    struct
+    {
+        D3D12_RESOURCE_STATES usage{ D3D12_RESOURCE_STATE_COMMON };
+        D3D12_RESOURCE_STATES transition{ (D3D12_RESOURCE_STATES)-1 };
+    } state;
+
+    D3D12_GPU_VIRTUAL_ADDRESS gpuVirtualAddress{ D3D12_GPU_VIRTUAL_ADDRESS_NULL };
+};
+
+}
+}
