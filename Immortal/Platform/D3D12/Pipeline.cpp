4f85ccaced885000780f425cf2929fdfeb2f07f3
Wu Jianhua <toqsxw@outlook.com>
2021-12-24 02:06:11 +0800

    D3D12/Pipeline: add pipeline construction
    Signed-off-by: Wu Jianhua <toqsxw@outlook.com>

--- Immortal/Platform/D3D12/Pipeline.cpp
+++ Immortal/Platform/D3D12/Pipeline.cpp
@@ -1,13 +1,16 @@
 #include "Pipeline.h"
 #include "Device.h"
 
+#include "RenderTarget.h"
 namespace Immortal
 {
 namespace D3D12
 {
 
 Pipeline::Pipeline(Device *device, std::shared_ptr<Shader::Super> shader) :
-    Super{ shader }
+    Super{ shader },
+    device{ device },
+    state{ new State{} }
 {
 
 }
@@ -15,6 +18,83 @@ Pipeline::Pipeline(Device *device, std::shared_ptr<Shader::Super> shader) :
 Pipeline::~Pipeline()
 {
     IfNotNullThenRelease(pipelineState);
+}
+
+void Pipeline::Set(const InputElementDescription &description)
+{
+    ThrowIf(!state, SError::NullPointerReference);
+
+    auto &inputElementDesc = state->InputElementDescription;
+    inputElementDesc.resize(description.Size());
+    for (size_t i = 0; i < description.Size(); i++)
+    {
+        inputElementDesc[i].SemanticName         = description[i].Name().c_str();
+        inputElementDesc[i].SemanticIndex        = 0;
+        inputElementDesc[i].Format               = description[i].BaseType<DXGI_FORMAT>();
+        inputElementDesc[i].InputSlot            = 0;
+        inputElementDesc[i].AlignedByteOffset    = description[i].Offset();
+        inputElementDesc[i].InputSlotClass       = D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA;
+        inputElementDesc[i].InstanceDataStepRate = 0;
+    }
+}
+
+void Pipeline::Create(const std::shared_ptr<RenderTarget::Super> &renderTarget)
+{
+    // Reconstruct(renderTarget);
+}
+
+void Pipeline::Reconstruct(const std::shared_ptr<RenderTarget::Super> &superRenderTarget)
+{
+    RootSignature::Description rootSignatureDesc{
+        0,
+        nullptr,
+        0,
+        nullptr,
+        RootSignature::Flag::AllowInputAssemblerInputLayout
+    };
+
+    ComPtr<ID3DBlob> signature;
+    ComPtr<ID3DBlob> error;
+    Check(D3D12SerializeRootSignature(&rootSignatureDesc, D3D_ROOT_SIGNATURE_VERSION_1, &signature, &error));
+    device->Create(0, signature.Get(), &rootSignature);
+
+    auto shader     = std::dynamic_pointer_cast<Shader>(desc.shader);
+    auto bytesCodes = shader->ByteCodes();
+
+    D3D12_INPUT_LAYOUT_DESC inputLayoutDesc = { 
+        state->InputElementDescription.data(),
+        U32(state->InputElementDescription.size())
+    };
+
+    D3D12_GRAPHICS_PIPELINE_STATE_DESC pipelineStateDesc{};
+    CleanUpObject(&pipelineStateDesc);
+    pipelineStateDesc.InputLayout                     = inputLayoutDesc;
+    pipelineStateDesc.pRootSignature                  = rootSignature;
+    pipelineStateDesc.VS                              = bytesCodes[Shader::VertexShaderPos];
+    pipelineStateDesc.PS                              = bytesCodes[Shader::PixelShaderPos ];
+    pipelineStateDesc.RasterizerState                 = RasterizerDescription{};
+    pipelineStateDesc.BlendState                      = BlendDescription{};
+    pipelineStateDesc.DepthStencilState.DepthEnable   = FALSE;
+    pipelineStateDesc.DepthStencilState.StencilEnable = FALSE;
+    pipelineStateDesc.SampleMask                      = UINT_MAX;
+    pipelineStateDesc.PrimitiveTopologyType           = SuperToBase(desc.PrimitiveType);
+    pipelineStateDesc.NumRenderTargets                = 1;
+    pipelineStateDesc.SampleDesc.Count                = 1;
+
+    auto renderTarget = std::dynamic_pointer_cast<RenderTarget>(superRenderTarget);
+    auto &colorBuffers = renderTarget->GetColorBuffers();
+
+    ThrowIf(colorBuffers.size() > SL_ARRAY_LENGTH(pipelineStateDesc.RTVFormats), SError::OutOfBound);
+    pipelineStateDesc.NumRenderTargets = colorBuffers.size();
+    for (int i = 0; i < pipelineStateDesc.NumRenderTargets; i++)
+    {
+        pipelineStateDesc.RTVFormats[i] = colorBuffers[i].Format;
+    }
+
+    auto &depthBuffer = renderTarget->GetDepthBuffer();
+    pipelineStateDesc.DSVFormat = depthBuffer.Format;
+
+    device->Create(&pipelineStateDesc, &pipelineState);
 }
 
 }
--- Immortal/Platform/D3D12/Pipeline.h
+++ Immortal/Platform/D3D12/Pipeline.h
@@ -5,6 +5,7 @@
 #include "Render/Pipeline.h"
 #include "Render/Texture.h"
 #include "Shader.h"
+#include "RootSignature.h"
 
 namespace Immortal
 {
@@ -17,13 +18,127 @@ class Pipeline : public SuperPipeline
 public:
     using Super = SuperPipeline;
 
+    struct State
+    {
+        std::vector<D3D12_INPUT_ELEMENT_DESC> InputElementDescription;
+    };
+
+    struct RasterizerDescription : public D3D12_RASTERIZER_DESC
+    {
+        using Primitive = D3D12_RASTERIZER_DESC;
+
+        RasterizerDescription()
+        {
+            Primitive::FillMode              = D3D12_FILL_MODE_SOLID;
+            Primitive::CullMode              = D3D12_CULL_MODE_BACK;
+            Primitive::FrontCounterClockwise = FALSE;
+            Primitive::DepthBias             = D3D12_DEFAULT_DEPTH_BIAS;
+            Primitive::DepthBiasClamp        = D3D12_DEFAULT_DEPTH_BIAS_CLAMP;
+            Primitive::SlopeScaledDepthBias  = D3D12_DEFAULT_SLOPE_SCALED_DEPTH_BIAS;
+            Primitive::DepthClipEnable       = TRUE;
+            Primitive::MultisampleEnable     = FALSE;
+            Primitive::AntialiasedLineEnable = FALSE;
+            Primitive::ForcedSampleCount     = 0;
+            Primitive::ConservativeRaster    = D3D12_CONSERVATIVE_RASTERIZATION_MODE_OFF;
+        }
+
+        explicit RasterizerDescription(const Primitive &o) noexcept :
+            Primitive{ o }
+        {
+        
+        }
+
+        explicit RasterizerDescription(
+            D3D12_FILL_MODE fillMode,
+            D3D12_CULL_MODE cullMode,
+            BOOL frontCounterClockwise,
+            INT depthBias,
+            FLOAT depthBiasClamp,
+            FLOAT slopeScaledDepthBias,
+            BOOL depthClipEnable,
+            BOOL multisampleEnable,
+            BOOL antialiasedLineEnable,
+            UINT forcedSampleCount,
+            D3D12_CONSERVATIVE_RASTERIZATION_MODE conservativeRaster) noexcept
+        {
+            Primitive::FillMode              = fillMode;
+            Primitive::CullMode              = cullMode;
+            Primitive::FrontCounterClockwise = frontCounterClockwise;
+            Primitive::DepthBias             = depthBias;
+            Primitive::DepthBiasClamp        = depthBiasClamp;
+            Primitive::SlopeScaledDepthBias  = slopeScaledDepthBias;
+            Primitive::DepthClipEnable       = depthClipEnable;
+            Primitive::MultisampleEnable     = multisampleEnable;
+            Primitive::AntialiasedLineEnable = antialiasedLineEnable;
+            Primitive::ForcedSampleCount     = forcedSampleCount;
+            Primitive::ConservativeRaster    = conservativeRaster;
+        }
+    };
+
+    struct BlendDescription : public D3D12_BLEND_DESC
+    {
+        using Primitive = D3D12_BLEND_DESC;
+
+        explicit BlendDescription() noexcept
+        {
+            Primitive::AlphaToCoverageEnable  = FALSE;
+            Primitive::IndependentBlendEnable = FALSE;
+            const D3D12_RENDER_TARGET_BLEND_DESC defaultRenderTargetBlendDescription =
+            {
+                FALSE,FALSE,
+                D3D12_BLEND_ONE, D3D12_BLEND_ZERO, D3D12_BLEND_OP_ADD,
+                D3D12_BLEND_ONE, D3D12_BLEND_ZERO, D3D12_BLEND_OP_ADD,
+                D3D12_LOGIC_OP_NOOP,
+                D3D12_COLOR_WRITE_ENABLE_ALL,
+            };
+            for (UINT i = 0; i < D3D12_SIMULTANEOUS_RENDER_TARGET_COUNT; ++i)
+            {
+                Primitive::RenderTarget[i] = defaultRenderTargetBlendDescription;
+            }
+        }
+
+        explicit BlendDescription(const D3D12_BLEND_DESC &o) noexcept :
+            Primitive{ o }
+        {
+        
+        }
+    };
+
+    static D3D12_PRIMITIVE_TOPOLOGY_TYPE SuperToBase(const PrimitiveType type)
+    {
+        switch (type)
+        {
+        case PrimitiveType::Line:
+            return D3D12_PRIMITIVE_TOPOLOGY_TYPE_LINE;
+
+        case PrimitiveType::Point:
+            return D3D12_PRIMITIVE_TOPOLOGY_TYPE_POINT;
+
+        case PrimitiveType::Triangles:
+        default:
+            return D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
+        }
+    }
+
 public:
     Pipeline(Device *device, std::shared_ptr<Shader::Super> shader);
 
     virtual ~Pipeline();
 
+    virtual void Create(const std::shared_ptr<SuperRenderTarget> &renderTarget) override;
+
+    virtual void Reconstruct(const std::shared_ptr<SuperRenderTarget> &renderTarget) override;
+
+    virtual void Set(const InputElementDescription &description) override;
+
 private:
+    Device *device{ nullptr };
+
     ID3D12PipelineState *pipelineState{ nullptr };
+
+    RootSignature rootSignature;
+
+    std::unique_ptr<State> state;
 };
 
 }
