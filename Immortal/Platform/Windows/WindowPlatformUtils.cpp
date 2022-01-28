d642e14d25a2a794f0c8db0229c664c3dba37192
Wu Jianhua <toqsxw@outlook.com>
2022-01-28 23:11:38 +0800

    Platform/Windows: remember the remember the last directory opened
    Signed-off-by: Wu Jianhua <toqsxw@outlook.com>

--- Immortal/Platform/Windows/WindowPlatformUtils.cpp
+++ Immortal/Platform/Windows/WindowPlatformUtils.cpp
@@ -6,31 +6,35 @@
 #include "Framework/Application.h"
 
 namespace Immortal
-{
+{ 
 
 std::optional<std::string> FileDialogs::OpenFile(const char *filter)
 {
+    static char lastDir[1024] = { 0 };
+
     OPENFILENAMEA ofn;
     CHAR szFile[260] = { 0 };
-    CHAR currentDir[256] = { 0 };
     ZeroMemory(&ofn, sizeof(OPENFILENAMEA));
     ofn.lStructSize = sizeof(OPENFILENAMEA);
     ofn.hwndOwner = reinterpret_cast<HWND>(Application::App()->GetWindow().Primitive());
 
     ofn.lpstrFile = szFile;
     ofn.nMaxFile = sizeof(szFile);
-    if (GetCurrentDirectoryA(sizeof(currentDir), currentDir))
+    if (lastDir[0] != '\0' || GetCurrentDirectoryA(sizeof(lastDir), lastDir))
     {
-        ofn.lpstrInitialDir = currentDir;
+        ofn.lpstrInitialDir = lastDir;
     }
+
     ofn.lpstrFilter = filter;
     ofn.nFilterIndex = 1;
     ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST | OFN_NOCHANGEDIR;
 
     if (GetOpenFileNameA(&ofn) == TRUE)
     {
+        strcat(lastDir, ofn.lpstrFile);
         return ofn.lpstrFile;
     }
+
     return std::nullopt;
 }
 
