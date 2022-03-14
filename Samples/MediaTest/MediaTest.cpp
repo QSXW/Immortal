d924cac52d6d132859b7fe5e088774aabbeb73e6
Wu Jianhua <toqsxw@outlook.com>
2022-03-14 21:14:22 +0800

    Samples: add example for media test
    Signed-off-by: Wu Jianhua <toqsxw@outlook.com>

---
+++ Samples/MediaTest/MediaTest.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "Immortal.h"
+#include "Media/Stream.h"
+#include "Media/Interface/Codec.h"
+#include "Media/Video/HEVC.h"
+#include "Media/Common/BitTracker.h"
+#include "Media/Image/Jpeg.h"
+
+using namespace Immortal;
+
+int main()
+{
+    LOG::Setup();
+
+    {
+        std::string path = "1920x800_25fps.265";
+        Stream stream{ path, Stream::Mode::Read };
+        THROWIF(!stream.Readable(), "Unable to open file");
+
+        std::vector<uint8_t> buffer;
+        buffer.reserve(SLALIGN(stream.Size(), 64));
+        buffer.resize(stream.Size());
+        stream.Read(buffer);
+
+        Vision::HEVCCodec decoder;
+        decoder.Decode(buffer);
+    }
+
+    return 0;
+}
