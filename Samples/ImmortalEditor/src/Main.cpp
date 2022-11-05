191cb58991ab0fc00c67368e6a2734984403cd08
Wu Jianhua <toqsxw@outlook.com>
2022-11-06 06:22:30 +0800

    Samples: remove OpenGLSample, D3D12Sample and VulkanSample
    Signed-off-by: Wu Jianhua <toqsxw@outlook.com>

---
+++ Samples/ImmortalEditor/src/Main.cpp
@@ -0,0 +1,39 @@
+/**
+ * Copyright (C) 2021-2022, by Wu Jianhua (toqsxw@outlook.com)
+ *
+ * This library is distributed under the Apache-2.0 license.
+ */
+
+#include "RenderLayer.h"
+
+using namespace Immortal;
+
+class ImmortalEditor : public Application
+{
+public:
+    ImmortalEditor() : Application({ "Immortal Editor", 1920, 1080 })
+    {
+        auto renderLayer = new RenderLayer(Vector2{ 1920, 1080 }, "Debug Layer for Render");
+        PushLayer(renderLayer);
+    }
+
+    ~ImmortalEditor()
+    {
+
+    }
+};
+
+int main(int argc, char **argv)
+{
+    LOG::Init();
+
+    {
+        Render::Set(Render::Type::Vulkan);
+        URef<Application> app{ new ImmortalEditor() };
+        app->Run();
+    }
+
+    LOG::Release();
+
+    return 0;
+}
