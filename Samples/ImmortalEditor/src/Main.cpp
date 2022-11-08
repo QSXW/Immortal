09cf9c0196792e5a63ddee128f49c9bef91cfbcf
Wu Jianhua <toqsxw@outlook.com>
2022-11-09 00:27:29 +0800

    Samples/ImmortalEditor: catch critical error exception
    Signed-off-by: Wu Jianhua <toqsxw@outlook.com>

--- Samples/ImmortalEditor/src/Main.cpp
+++ Samples/ImmortalEditor/src/Main.cpp
@@ -27,10 +27,16 @@ int main(int argc, char **argv)
 {
     LOG::Init();
 
+    try
     {
         Render::Set(Render::Type::Vulkan);
         URef<Application> app{ new ImmortalEditor() };
         app->Run();
+    }
+    catch (const std::exception &e)
+    {
+        LOG::FATAL("Critical Error Detected: {}", e.what());
+        LOG::FATAL("The program {} will be exited!", argv[0]);
     }
 
     LOG::Release();
